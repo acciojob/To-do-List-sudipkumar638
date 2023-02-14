03377289f9bf5b3d8e8a259b79587aa855a950e3
sudipkumar638 <93931722+sudipkumar638@users.noreply.github.com>
2023-02-14 13:54:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-//your code here
+var textbox=document.getElementById("newTodoInput");
 
+var but=document.getElementById("addTodoBtn");
+var list=document.getElementById("todoList");
+but.addEventListener("click",()=>{
+	if(document.getElementById("newTodoInput").value.trim()!="")
+	{
+		list.innerHTML=`${list.innerHTML}<li>${document.getElementById("newTodoInput").value}</li>`
+	document.getElementById("newTodoInput").value=""
+	}
+   
+	
+})
